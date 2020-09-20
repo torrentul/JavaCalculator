006581e72e3c02bda06926fd73232282b3df595c
torrentul <ya.totikus@yandex.ru>
2020-09-20 23:40:43 +0300

    Bug Fix. Roman numbers multiplication.

--- src/com/company/ArabicToRom.java
+++ src/com/company/ArabicToRom.java
@@ -8,6 +8,7 @@ public class ArabicToRom {
     private final List<String> enumbers = new ArrayList<>();
 
     public String getRomNumber(int number) {
+        enumbers.addAll(Collections.singleton(""));
         enumbers.addAll(Collections.singleton(Numbers.I.toString()));
         enumbers.addAll(Collections.singleton(Numbers.II.toString()));
         enumbers.addAll(Collections.singleton(Numbers.III.toString()));
@@ -19,7 +20,35 @@ public class ArabicToRom {
         enumbers.addAll(Collections.singleton(Numbers.IX.toString()));
         enumbers.addAll(Collections.singleton(Numbers.X.toString()));
 
-        String res = enumbers.get(number-1);
+        String res = "";
+        int count = (number-(number%10))/10;
+        if (number<11){
+            res = enumbers.get(number);
+        }else if (number < 40){
+            int dec=0;
+            for (int i=0;i<count;i++){
+                res=res+"X";
+                dec++;
+            }
+            res=res+enumbers.get(number%10);
+        }else if (number < 50){
+
+            res="XL"+enumbers.get(number%10);
+        }else if (number < 90){
+            res="L";
+            for (int i=0;i<count-5;i++){
+                res=res+"X"; }
+            res=res+enumbers.get(number%10);
+        }else if (number < 100){
+            res="XC"+enumbers.get(number%10);
+        }else {
+            res="C";
+        }
+
+
+
+
+
         return res;
     }
 }
--- src/com/company/Processor.java
+++ src/com/company/Processor.java
@@ -3,36 +3,32 @@ package com.company;
 public class Processor {
 
     public String calculate (String a){
-        String result = null;
+        String result = "Error";
         String[] strings = a.split(" ");
         int number1 ;
         int number2;
         try {
             number1=Integer.parseInt(strings[0]);
             number2=Integer.parseInt(strings[2]);
-
+            MathOperator mathOperator = new MathOperator();
+            String res =mathOperator.calculate(strings[1],number1,number2);
+            return result=res;
         }catch (Exception e){
             try {
                 RomanToArabic romanToArabic = new RomanToArabic();
                 number1 = romanToArabic.convert(strings[0]);
                 number2 = romanToArabic.convert(strings[2]);
+                MathOperator mathOperator = new MathOperator();
+                String res =mathOperator.calculate(strings[1],number1,number2);
+                ArabicToRom arabicToRom = new ArabicToRom();
+                result=arabicToRom.getRomNumber(Integer.parseInt(res));
+                return result;
             }catch (Exception exception){
                 throw exception;
             }
         }
-        try {
-            MathOperator mathOperator = new MathOperator();
-            String res =mathOperator.calculate(strings[1],number1,number2);
-            ArabicToRom arabicToRom = new ArabicToRom();
-            result=arabicToRom.getRomNumber(Integer.parseInt(res));
-
-        }catch (Exception e){
-
-        }
 
 
-
-        return result;
     }
 
 
